365e4592a303b9aae799e6ac1dacb07fc3a1fa4e
Areum Jo <areumjo1@gmail.com>
2019-07-12 12:00:29 -0400

    Got article data and created article cards using the data

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,52 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(data => {
+        console.log('checking if it works:', data.data.articles);
+        const articleObj = data.data.articles;
+        const js = articleObj.javascript;
+        const bs = articleObj.bootstrap;
+        const tech = articleObj.technology;
+        const jq = articleObj.jquery;
+        const node = articleObj.node;
+        console.log(js, bs,tech, jq,node)
+        js.forEach( ele => cardCompo.appendChild(addArticle(ele)));
+        bs.forEach( ele => cardCompo.appendChild(addArticle(ele)));
+        tech.forEach( ele => cardCompo.appendChild(addArticle(ele)));
+        jq.forEach( ele => cardCompo.appendChild(addArticle(ele)));
+        node.forEach( ele => cardCompo.appendChild(addArticle(ele)));
+    })
+    .catch(error => {
+        console.log('API is currently down, try again later', error);
+    })
+
+const cardCompo = document.querySelector('.cards-container');
+
+function addArticle(obj) {
+    const card = document.createElement('div');
+    
+    const title = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const imgAuthor = document.createElement('img');
+    const spanAuthor = document.createElement('span');
+
+    card.classList.add('card');
+    title.classList.add('headline');
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    title.textContent=obj.headline;
+    imgAuthor.src=obj.authorPhoto;
+    spanAuthor.textContent=`By ${obj.authorName}`;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(imgAuthor);
+    author.appendChild(spanAuthor);
+
+    return card;
+}
